08a730adda7b356654f306de9bb0125ffaade004
Balaji Renganathan <Balaji_mcr@yahoo.com>
2023-05-07 00:15:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,30 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+# Set page title and favicon
+st.set_page_config(page_title="Marriage Compatibility Test", page_icon=":heart:")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Define questions
+questions = [
+    "I feel loved and cared for by my partner.",
+    "I feel comfortable sharing my thoughts and feelings with my partner.",
+    "I trust my partner and feel secure in our relationship.",
+    "My partner and I have similar values and goals in life."
+]
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Define answer options
+answer_options = ["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"]
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Define dictionary to store answers
+answers = {}
 
+# Display each question and collect answers
+for i, question in enumerate(questions):
+    st.write(f"**{i+1}. {question}**")
+    selected_option = st.radio("", answer_options)
+    answers[question] = selected_option
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Display submitted answers
+if st.button("Submit"):
+    st.write("Here are your submitted answers:")
+    for question, answer in answers.items():
+        st.write(f"- {question}: {answer}")
